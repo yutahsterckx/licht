9fff3d471ee772b30d1b5b5b190ae594eddeca14
yutahsterckx <167744748+yutahsterckx@users.noreply.github.com>
2024-05-13 09:28:14 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.B, function () {
+    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
+})
+basic.forever(function () {
+	
+})
